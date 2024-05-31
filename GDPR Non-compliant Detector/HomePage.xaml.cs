f9579d0f578d9b5477b0e2aa16b401c49349f11c
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-05-31 20:47:46 +0200

    The Frontend

---
+++ GDPR Non-compliant Detector/HomePage.xaml.cs
@@ -0,0 +1,39 @@
+namespace GDPR_Non_compliant_Detector;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnFilesButtonClicked(object sender, EventArgs e)
+    {
+		await DisplayAlert("Files", "Files button clicked.", "Ok");
+    }
+
+    private async void OnUserManualButtonClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("User-Manual", "User-manual button clicked.", "Ok");
+    }
+
+    private async void OnSettingsButtonClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Settings", "Settings button clicked.", "Ok");
+    }
+
+    private async void OnHelpButtonClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Help", "help button clicked.", "Ok");
+    }
+
+    private async void OnDetailsClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Details", "Details button clicked.", "Ok");
+    }
+
+    private async void OnScanClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Scan", "Scan button clicked.", "Ok");
+    }
+}
