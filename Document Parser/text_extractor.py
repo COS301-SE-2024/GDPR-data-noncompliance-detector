1089a4401630ae80be216dff9f1b589b7e955298
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-01 14:25:07 +0200

    Basic File Agnostic

--- Document Parser/text_extractor.py
+++ Document Parser/text_extractor.py
@@ -1,10 +1,35 @@
+import sys
+import pandas as pd
 from pdfminer.high_level import extract_text
+from docx import Document
 
 def extract_text_from_pdf(file_path):
     return extract_text(file_path)
 
-path = "NCE1.pdf"
-text = extract_text_from_pdf(path)
+def extract_text_from_docx(file_path):
+    doc = Document(file_path)
+    return ' '.join([paragraph.text for paragraph in doc.paragraphs])
 
-with open('output.txt', 'w') as f:
-    f.write(text)
+def extract_data_from_excel(file_path):
+    df = pd.read_excel(file_path)
+    return df.to_string(index=False)
+
+if __name__ == "__main__":
+    path = "NCEWD1.docx"
+    if path.lower().endswith('.pdf'):
+        text = extract_text_from_pdf(path)
+    elif path.lower().endswith('.docx'):
+        text = extract_text_from_docx(path)
+    elif path.lower().endswith(('.xlsx', '.xls')):
+        text = extract_data_from_excel(path)
+    else:
+        print("Unsupported file type")
+        sys.exit(1)
+
+    with open('output.txt', 'w') as f:
+        f.write(text)
+
+#pip install document
+#pip install PdfFileReader
+#pip install PyPDF2
+#pip install python-docx
