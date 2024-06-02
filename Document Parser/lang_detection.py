9be39c59de5d9d434914eff756a7a6aad5c7c49b
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-03 00:59:05 +0200

    class seaparation

---
+++ Document Parser/lang_detection.py
@@ -0,0 +1,41 @@
+from langdetect import detect_langs
+from langcodes import Language
+
+class location_finder:
+    def detect_country(self, file):
+        with open(file, 'r', encoding='utf-8') as file:
+            data = file.read()
+
+        try:
+            languages = detect_langs(data)
+            # primary_language = str(languages[0]).split(':')[0]
+            # return primary_language
+
+            possible_languages = []
+
+            for language in languages:
+                country_code = language.lang
+                full_country_name = Language.make(country_code).display_name()
+                possible_languages.append((full_country_name, language.prob))
+
+            return possible_languages
+
+        except Exception as e:
+            print("Error:", e)
+            return None
+
+# def main():
+    
+#     file = "../../mock_data/language_data/polish.txt"
+#     countries = detect_country(file)
+
+#     if countries:
+#         print("Most probable countries of origin:")
+#         for country in countries:
+#             print(f'Country: {country[0]}, Probability: {country[1]}')
+
+#     else:
+#         print("Could not determine the country of origin.")
+
+# if __name__ == "__main__":
+#     main()
--- Document Parser/main.py
+++ Document Parser/main.py
@@ -1,12 +1,23 @@
 from document_parser import document_parser
+from lang_detection import location_finder
 import sys
 
 def main():
 
     path = input("File Name: ")
     parser = document_parser(path)
-    path = parser.process()
-    print(path)
+    file = parser.process()
+    print(file)
+    locale_search = location_finder()
+    countries = locale_search.detect_country(file)
+
+    if countries:
+        print("Most probable countries of origin:")
+        for country in countries:
+            print(f'Country: {country[0]}, Probability: {country[1]}')
+
+    else:
+        print("Could not determine the country of origin.")
     
 
 if __name__ == "__main__":
