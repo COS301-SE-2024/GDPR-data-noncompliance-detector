8c478787e120b3be0bc80f025ce4b9191418f079
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-06 22:24:13 +0200

    encoding preprocessor added

--- backend/Document_parser/text_extractor.py
+++ backend/Document_parser/text_extractor.py
@@ -1,14 +1,40 @@
 import pandas as pd
 from pdfminer.high_level import extract_text
 from docx import Document
-
-
+import fitz
+import pytesseract
+from PIL import Image
+import io
+import pdfplumber
 class text_extractor:
     def __init__(self):
         self.ext = ''
 
     def extract_text_from_pdf(self, file_path):
-        return extract_text(file_path)
+        combined_text = []
+
+        with pdfplumber.open(file_path) as pdf:
+            for page_num, page in enumerate(pdf.pages):
+                text = page.extract_text()
+                if text:
+                    combined_text.append(text)
+        
+        pdf_document = fitz.open(file_path)
+
+        for page_num in range(len(pdf_document)):
+            page = pdf_document.load_page(page_num)
+            images = page.get_images(full=True)
+            
+            for img_index, img in enumerate(images):
+                xref = img[0]
+                base_image = pdf_document.extract_image(xref)
+                image_bytes = base_image["image"]
+                image = Image.open(io.BytesIO(image_bytes))
+
+                ocr_text = pytesseract.image_to_string(image)
+                combined_text.append(ocr_text)
+
+        return "\n".join(combined_text)
 
     def extract_text_from_docx(self, file_path):
         doc = Document(file_path)
