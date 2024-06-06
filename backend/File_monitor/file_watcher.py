85ab868f374f706e38c9f8f02ad89ea17bc0868a
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-06-06 16:54:38 +0200

    runs with command

--- backend/File_monitor/file_watcher.py
+++ backend/File_monitor/file_watcher.py
@@ -2,18 +2,45 @@ import sys
 import time
 import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+
+
+def check_file_extension(filename, reg):
+    for str in reg:
+        if filename.endswith(f".{str}"):
+            return True
+
+
+class handle(FileSystemEventHandler):
+    def __init__(self, file_extension):
+        self.file_extension = file_extension
+
+    def on_modified(self, event):
+        # print(f'{event.event_type}  path : {event.src_path}')
+        if (check_file_extension(event.src_path, self.file_extension)):
+            print(f"modified:{event.src_path}")
+
+    def on_created(self, event):
+        # print(f'{event.event_type}  path : {event.src_path}')
+        if (check_file_extension(event.src_path, self.file_extension)):
+            print(f"created:{event.src_path}")
+
+    # def on_deleted(self, event):
+    #     print(f'{event.event_type}  path : {event.src_path}')
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1]
+    ext = sys.argv[2]
+    extparams = ext.split(',')
     logging.info(f'start watching directory {path!r}')
-    event_handler = LoggingEventHandler()
+    
+    event_handler = handle(extparams)
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path, recursive=True) # watches subfolders
     observer.start()
+
     try:
         while True:
             time.sleep(1)
