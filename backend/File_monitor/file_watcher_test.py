0b8faffaa686af8a5bcc536d910271db24ea4c94
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-06-07 17:48:06 +0200

    filewatcher linting

--- backend/File_monitor/file_watcher_test.py
+++ backend/File_monitor/file_watcher_test.py
@@ -11,27 +11,27 @@ class TestFileWatcher(unittest.TestCase):
         self.event_handler = file_watcher.handle(self.extensions)
 
     def test_on_modified_with_valid_extension(self):
-        event = FileModifiedEvent('{wd}/file.txt')
+        event = FileModifiedEvent('/file.txt')
         response = self.event_handler.on_modified(event)
-        wd = os.getcwd()
-        expected_response = json.dumps({"type": "modified", "path": "{wd}/file.txt"})
+        expected_response = json.dumps({"type": "modified", "path": "/file.txt"})
         self.assertEqual(response, expected_response)
 
     def test_on_modified_with_invalid_extension(self):
-        event = FileModifiedEvent('{wd}/file.jpg')
+        event = FileModifiedEvent('/file.jpg')
         response = self.event_handler.on_modified(event)
         self.assertIsNone(response)
 
     def test_on_created_with_valid_extension(self):
-        event = FileCreatedEvent('{wd}/file.pdf')
+        event = FileCreatedEvent('/file.pdf')
         response = self.event_handler.on_created(event)
-        expected_response = json.dumps({"type": "created", "path": "{wd}/file.pdf"})
+        expected_response = json.dumps({"type": "created", "path": "/file.pdf"})
         self.assertEqual(response, expected_response)
 
     def test_on_created_with_invalid_extension(self):
-        event = FileCreatedEvent('{wd}/file.bmp')
+        event = FileCreatedEvent('/file.bmp')
         response = self.event_handler.on_created(event)
         self.assertIsNone(response)
+
 
 if __name__ == '__main__':
     unittest.main()
