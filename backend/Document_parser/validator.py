205f221adc99a87a5560e5b5d9b8a0c6fa2a749a
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-07 18:52:50 +0200

    Basic virus scanning

--- backend/Document_parser/validator.py
+++ backend/Document_parser/validator.py
@@ -1,11 +1,23 @@
 import os
-
+import subprocess as sp
 
 class validator:
+
+    def anti_virus(self,file_path):
+        try:
+            command = f"Start-MpScan -ScanPath {file_path} -ScanType QuickScan"
+            sp.run(["powershell","-Command",command], check=True)
+        except sp.CalledProcessError as e:
+            raise RuntimeError(f"Virus Scan Failed : {e}")
+
+
     def process_file(self, file_path):
         try:
             if not os.path.exists(file_path):
                 raise FileNotFoundError(f"File not found: {file_path}")
+
+            print("Initial Virus Scan :")
+            self.anti_virus(file_path)
 
             extension = os.path.splitext(file_path)[1].lower()
             if extension == '.pdf':
