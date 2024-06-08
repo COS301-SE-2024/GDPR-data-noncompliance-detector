1510d507d876a20bf9607c03890a6146f8335084
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-08 18:48:32 +0200

    Deactivated virus scanning

--- backend/Document_parser/validator.py
+++ backend/Document_parser/validator.py
@@ -3,12 +3,16 @@ import subprocess as sp
 
 class validator:
 
-    def anti_virus(self,file_path):
-        try:
-            command = f"Start-MpScan -ScanPath {file_path} -ScanType QuickScan"
-            sp.run(["powershell","-Command",command], check=True)
-        except sp.CalledProcessError as e:
-            raise RuntimeError(f"Virus Scan Failed : {e}")
+    # def anti_virus(self,file_path):
+    #     try:
+    #         mp_cmd_run = "C:\\ProgramData\\Microsoft\\Windows Defender\\Platform\\*\\MpCmdRun.exe"
+    #         command = f"{mp_cmd_run} -Scan -ScanType 3 -File {file_path} -DisableRemediation"
+            
+    #         result = sp.run(["powershell", "-Command", command], capture_output=True, text=True, check=True)
+            
+    #         print(result.stdout)
+    #     except sp.CalledProcessError as e:
+    #         raise RuntimeError(f"Virus Scan Failed : {e}")
 
 
     def process_file(self, file_path):
@@ -16,8 +20,8 @@ class validator:
             if not os.path.exists(file_path):
                 raise FileNotFoundError(f"File not found: {file_path}")
 
-            print("Initial Virus Scan :")
-            self.anti_virus(file_path)
+            # print("Initial Virus Scan :")
+            # self.anti_virus(file_path)
 
             extension = os.path.splitext(file_path)[1].lower()
             if extension == '.pdf':
