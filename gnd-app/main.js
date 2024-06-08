8571e0dc364dc9a2dd4f4546765d384bd7800239
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-06-08 02:54:12 +0200

    chore: Update Angular and Electron dependencies
    Also made the navbar been fighting with the configuration because when I serve the Angular app it runs properly what I have coded but when I run it with electron is shows old things

--- gnd-app/main.js
+++ gnd-app/main.js
@@ -1,16 +1,38 @@
-const {app, BrowserWindow} = require('electron');  
-const url = require('url');
-const path = require('path');   
-	
-function onReady () {     
-	win = new BrowserWindow({width: 900, height: 6700})    
-	win.loadURL(url.format({      
-		pathname: path.join(
-			__dirname,
-			'dist/gnd-app/index.html'),       
-		protocol: 'file:',      
-		slashes: true     
-	}))   
-} 
+// let win; // Declare win at the top of the file
 
-app.on('ready', onReady);
+// const {app, BrowserWindow} = require('electron');
+// const url = require('url');
+// const path = require('path');
+
+// // Require electron-reload and pass the directory to watch
+// require('electron-reload')(__dirname, {
+//     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+// });
+
+// function onReady () {
+//     win = new BrowserWindow({width: 900, height: 600}) // Adjust the height
+//     win.loadURL(url.format({
+//         pathname: path.join(
+//             __dirname,
+//             'dist/gnd-app/index.html'),
+//         protocol: 'file:',
+//         slashes: true
+//     }))
+// }
+
+// app.on('ready', onReady);
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+
+  win.loadFile('dist/gnd-app/index.html')
+}
+
+app.whenReady().then(createWindow)
