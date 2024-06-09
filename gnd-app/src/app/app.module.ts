aebaa69f0f31e84cfb9bf5f036b5e0669b8b5117
Yeshlen Moodley <yeshlenmoodley01@gmail.com>
2024-06-09 13:28:16 +0200

    Implemented Inbox Component

--- gnd-app/src/app/app.module.ts
+++ gnd-app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ gnd-app/src/app/inbox/inbox.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-inbox',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './inbox.component.html',
+  styleUrl: './inbox.component.css'
+})
+export class InboxComponent {
+
+  mockData: any = {
+    email1: {rating: 80, origin: 'Netherlands', violationAreas: 'Personal Information', numViolations: 16},
+    email2: {rating: 37, origin: 'France', violationAreas: 'Banking Details', numViolations: 29},
+    email3: {rating: 54, origin: 'Greece', violationAreas: 'Personal Information', numViolations: 6},
+    email4: {rating: 37, origin: 'England', violationAreas: 'Banking Details', numViolations: 9},
+    email5: {rating: 37, origin: 'Germany', violationAreas: 'Personal Information', numViolations: 19},
+    email6: {rating: 37, origin: 'Spain', violationAreas: 'Banking Details', numViolations: 12},
+  };
+
+  //Initialise empty array as no data yet
+  currentAnalysis: any = {};
+  currentEmail: string = "";                                                   
+
+  showAnalysis(data: string) {
+    this.currentAnalysis = this.mockData[data];
+    this.currentEmail = data;
+  }
+
+  isObjectEmpty(obj: any) {
+    return Object.keys(obj).length === 0;
+  }
+
+  clearAnalysis() {
+    this.currentAnalysis = {};
+    this.currentEmail = "";
+  }
+}
