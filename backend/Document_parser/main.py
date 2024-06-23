bf59cdf94738ec06d15008b82bc284e78a4b177f
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-23 18:40:34 +0200

    fitz removal

--- backend/Document_parser/main.py
+++ backend/Document_parser/main.py
@@ -10,34 +10,30 @@ import pdfplumber
 import openpyxl #we need this for excel
 from openpyxl_image_loader import SheetImageLoader
 
+from pdf2image import convert_from_path
+# from docx.shared import Inches
+
 def extract_text_from_pdf(file_path):
-        combined_text = []
+    combined_text = []
 
-        with pdfplumber.open(file_path) as pdf:
-            for page_num, page in enumerate(pdf.pages):
-                text = page.extract_text()
-                if text:
-                    combined_text.append(text)
-        
-        pdf_document = fitz.open(file_path)
+    # Extract text using pdfplumber
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                combined_text.append(text)
 
-        for page_num in range(len(pdf_document)):
-            page = pdf_document.load_page(page_num)
-            images = page.get_images(full=True)
-            
-            for img_index, img in enumerate(images):
-                xref = img[0]
-                base_image = pdf_document.extract_image(xref)
-                image_bytes = base_image["image"]
-                image = Image.open(io.BytesIO(image_bytes))
+        # Convert PDF pages to images using pdf2image
+        images = convert_from_path(file_path)
 
-                ocr_text = pytesseract.image_to_string(image)
-                combined_text.append(ocr_text)
+        # Perform OCR on each image using pytesseract
+        for image in images:
+            ocr_text = pytesseract.image_to_string(image)
+            combined_text.append(ocr_text)
 
         return "\n".join(combined_text)
-
+        
 def main():
-    
     extracted_text = extract_text_from_pdf('COS332_EXAM_2022PDF_240617_194915.pdf')
     print(extracted_text)
 
