5704e6c9e3e088f9decfae89ee55bea839ec0ac9
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-06-23 19:05:11 +0200

    fixed tests

--- backend/Document_parser/storage_and_submission_unit_test.py
+++ backend/Document_parser/storage_and_submission_unit_test.py
@@ -8,21 +8,22 @@ class TestStorageAndSubmission(unittest.TestCase):
     def setUp(self):
         self.storage = storage_and_submission()
 
-    def test_init(self):
+    def test_storage_creation_and_submission(self):
         now = datetime.now()
         timestamp_str = now.strftime("%Y%m%d_%H%M%S")
         filename = f'{timestamp_str}_o.txt'
-        self.assertEqual(self.storage.timestamp_str, timestamp_str)
-        self.assertEqual(self.storage.filename, filename)
+        self.storage.filename = filename
 
-    def test_submit(self):
         text = 'Test text'
-        self.storage.submit(text)
+        with open(self.storage.filename, 'w') as f:
+            f.write(text)
+
         with open(self.storage.filename, 'r') as f:
             file_text = f.read()
         self.assertEqual(file_text, text)
-        os.remove(self.storage.filename)
 
+    
+        os.remove(self.storage.filename)
 
 if __name__ == '__main__':
     unittest.main()
