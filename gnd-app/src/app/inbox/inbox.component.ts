5c8244c3d15c9fc6136f81bf59baeb7d69dd8740
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-07-09 09:17:59 +0200

    updated inbox component

--- gnd-app/src/app/inbox/inbox.component.ts
+++ gnd-app/src/app/inbox/inbox.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FileService } from '../services/file.service';
 
 @Component({
   selector: 'app-inbox',
@@ -9,33 +10,49 @@ import { RouterModule } from '@angular/router';
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.css']
 })
-export class InboxComponent {
-  mockData: any = {
-    email1: {rating: 80, origin: 'Netherlands', violationAreas: 'Personal Information', numViolations: 16, fileType: 'pdf'},
-    email2: {rating: 37, origin: 'France', violationAreas: 'Banking Details', numViolations: 29, fileType: 'doc'},
-    email3: {rating: 54, origin: 'Greece', violationAreas: 'Personal Information', numViolations: 6, fileType: 'xlsx'},
-    email4: {rating: 37, origin: 'England', violationAreas: 'Banking Details', numViolations: 9, fileType: 'xlsx'},
-    email5: {rating: 37, origin: 'Germany', violationAreas: 'Personal Information', numViolations: 19, fileType: 'pdf'},
-    email6: {rating: 37, origin: 'Spain', violationAreas: 'Banking Details', numViolations: 12, fileType: 'txt'},
-  };
+export class InboxComponent implements OnInit {
+  reports: string[] = [];
+  path: string = 'C:/Users/Mervyn Rangasamy/Documents/Receiver';
+  
+  constructor(private fileService: FileService) { }
 
-  currentAnalysis: any = {};
-  currentEmail: string = "";  
-  currentEmailType: string = ""; // Add this line to track file type
-
-  showAnalysis(data: string) {
-    this.currentAnalysis = this.mockData[data];
-    this.currentEmail = data;
-    this.currentEmailType = this.mockData[data].fileType; // Set file type
+  ngOnInit(): void {
+    this.getReports();
   }
 
-  isObjectEmpty(obj: any) {
-    return Object.keys(obj).length === 0;
+  getReports(): void{
+    this.fileService.getFiles(this.path).subscribe(r => {
+      this.reports = r;
+    });
   }
 
-  clearAnalysis() {
-    this.currentAnalysis = {};
-    this.currentEmail = "";
-    this.currentEmailType = ""; // Clear file type
-  }
+
+  // mockData: any = {
+  //   email1: {rating: 80, origin: 'Netherlands', violationAreas: 'Personal Information', numViolations: 16, fileType: 'pdf'},
+  //   email2: {rating: 37, origin: 'France', violationAreas: 'Banking Details', numViolations: 29, fileType: 'doc'},
+  //   email3: {rating: 54, origin: 'Greece', violationAreas: 'Personal Information', numViolations: 6, fileType: 'xlsx'},
+  //   email4: {rating: 37, origin: 'England', violationAreas: 'Banking Details', numViolations: 9, fileType: 'xlsx'},
+  //   email5: {rating: 37, origin: 'Germany', violationAreas: 'Personal Information', numViolations: 19, fileType: 'pdf'},
+  //   email6: {rating: 37, origin: 'Spain', violationAreas: 'Banking Details', numViolations: 12, fileType: 'txt'},
+  // };
+
+  // currentAnalysis: any = {};
+  // currentEmail: string = "";  
+  // currentEmailType: string = ""; // Add this line to track file type
+
+  // showAnalysis(data: string) {
+  //   this.currentAnalysis = this.mockData[data];
+  //   this.currentEmail = data;
+  //   this.currentEmailType = this.mockData[data].fileType; // Set file type
+  // }
+
+  // isObjectEmpty(obj: any) {
+  //   return Object.keys(obj).length === 0;
+  // }
+
+  // clearAnalysis() {
+  //   this.currentAnalysis = {};
+  //   this.currentEmail = "";
+  //   this.currentEmailType = ""; // Clear file type
+  // }
 }
