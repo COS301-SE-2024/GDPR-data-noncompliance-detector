245c73b8709b18cdb40afb808201c6efe3d2cfa2
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-07-09 09:11:45 +0200

    updated services

--- gnd-app/src/app/services/file.service.ts
+++ gnd-app/src/app/services/file.service.ts
@@ -1,21 +1,16 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// interface FilePathResponse {
-//   status: string;
-//   path: string;
-// }
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+  private apiUrl = 'http://127.0.0.1:8000/reports';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class FileService {
-//   private apiUrl = 'http://127.0.0.1:8000/file-upload';
+  constructor(private http: HttpClient) {}
 
-//   constructor(private http: HttpClient) {}
-
-//   postNewFile(filePath: string): Observable<FilePathResponse> {
-//     return this.http.post<FilePathResponse>(this.apiUrl, { path: filePath });
-//   }
-// }
+  getFiles(directory: string): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}?directory=${directory}`);
+  }
+}
