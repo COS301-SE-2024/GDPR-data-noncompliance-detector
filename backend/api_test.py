00bd780ddcfeefb9f27757ea9154322c806ce3b9
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-07-11 11:08:17 +0200

    removed testing buttons

--- backend/api_test.py
+++ backend/api_test.py
@@ -1,9 +1,12 @@
 import requests
 
 if __name__ == "__main__":
-    url = 'http://127.0.0.1:8000/read-report/'
-    pars = {'path' : './Reports/dutch.txt'}
-    res =  requests.get(url=url,params=pars)
-    x = res.json()
-    print(res)
-    print(x['content'])
+    # url = 'http://127.0.0.1:8000/read-report/'
+    # pars = {'path' : './Reports/dutch.txt'}
+    # res =  requests.get(url=url,params=pars)
+    # x = res.json()
+    # print(res)
+    # print(x['content'])
+
+    url = 'http://127.0.0.1:8000/new-file/'
+
