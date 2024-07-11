475ca65e72cd49b7a771af8bf418141211fbd4fc
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-07-11 17:14:54 +0200

    biometric model working with local files

--- backend/Detection_Engine/biometric_detection.py
+++ backend/Detection_Engine/biometric_detection.py
@@ -3,8 +3,15 @@ import torch
 from PIL import Image
 import requests
 
-url = "./p1.png"
+# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+# image = Image.open(requests.get(url, stream=True).raw)
+# image.show()
+
+url = "../mockdata/p2.png"
 image = Image.open(url)
+if image.mode != 'RGB':
+    image = image.convert('RGB')
+image.show()
 
 
 processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
@@ -20,6 +27,6 @@ results = processor.post_process_object_detection(outputs, target_sizes=target_s
 for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
     box = [round(i, 2) for i in box.tolist()]
     print(
-            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"Bio {model.config.id2label[label.item()]} with confidence "
             f"{round(score.item(), 3)} at location {box}"
     )
