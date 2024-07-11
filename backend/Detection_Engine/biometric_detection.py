2060c3def35c634698cdf1b73005de67edc49ef9
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-07-11 17:21:58 +0200

    formatting to json

--- backend/Detection_Engine/biometric_detection.py
+++ backend/Detection_Engine/biometric_detection.py
@@ -1,32 +1,30 @@
 from transformers import DetrImageProcessor, DetrForObjectDetection
 import torch
 from PIL import Image
-import requests
 
-# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-# image = Image.open(requests.get(url, stream=True).raw)
-# image.show()
+def biometric_detect_people(source):
+    image = Image.open(source)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    # image.show()
 
-url = "../mockdata/p2.png"
-image = Image.open(url)
-if image.mode != 'RGB':
-    image = image.convert('RGB')
-image.show()
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
 
-
-processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
 
 
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+    output = []
 
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    print(
-            f"Bio {model.config.id2label[label.item()]} with confidence "
-            f"{round(score.item(), 3)} at location {box}"
-    )
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        print({"label": model.config.id2label[label.item()], "confidence": round(score.item(), 3)})
+        output.append({"label": model.config.id2label[label.item()], "confidence": round(score.item(), 3)})
+
+    return output
+
+if __name__ == "__main__":
+    biometric_detect_people("../mockdata/p2.png")
