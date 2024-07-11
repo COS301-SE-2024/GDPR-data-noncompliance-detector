c70816ac66ae227c3ab5516f14071a0345999f05
Nevin-Thomas <106274286+Nevin-Thomas@users.noreply.github.com>
2024-07-11 16:51:44 +0200

    Created faq-page component

---
+++ gnd-app/src/app/faq-page/faq-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './faq-page.component.html',
+  styleUrl: './faq-page.component.css'
+})
+export class FaqPageComponent {
+
+}
