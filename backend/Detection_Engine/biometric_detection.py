97e132f7868d676603246ce1fa762a1c34261291
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-07-11 17:49:36 +0200

    formatting

--- backend/Detection_Engine/biometric_detection.py
+++ backend/Detection_Engine/biometric_detection.py
@@ -16,15 +16,16 @@ def biometric_detect_people(source):
 
 
     target_sizes = torch.tensor([image.size[::-1]])
-    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.8)[0]
     output = []
 
     for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
         box = [round(i, 2) for i in box.tolist()]
-        print({"label": model.config.id2label[label.item()], "confidence": round(score.item(), 3)})
         output.append({"label": model.config.id2label[label.item()], "confidence": round(score.item(), 3)})
 
     return output
 
+
 if __name__ == "__main__":
-    biometric_detect_people("../mockdata/p2.png")
+    out = biometric_detect_people("../mockdata/p3.png")
+    print(out)
