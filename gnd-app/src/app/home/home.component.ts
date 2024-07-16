f0f3b48eaa6f50fe2b5e972e5dadd39e37e76ffe
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-07-16 18:48:18 +0200

    The app walkthrough using introjs
    The walkthrough shows/tells the user what button does

--- gnd-app/src/app/home/home.component.ts
+++ gnd-app/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import * as introJs from 'intro.js/intro.js';
+import {WalkthroughService} from '../services/walkthrough.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -9,6 +11,46 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private walkthroughSubscription?: Subscription;
 
+  constructor(private walkthroughService: WalkthroughService) {
+  }
+
+  ngOnInit() {
+    // Check if the user has seen the intro before
+    const hasSeenIntro = localStorage.getItem('hasSeenIntro');
+    if (!hasSeenIntro) {
+      this.startIntro();
+      // Mark that the user has seen the intro
+      localStorage.setItem('hasSeenIntro', 'true');
+    }
+    this.walkthroughSubscription = this.walkthroughService.walkthroughRequested$.subscribe(()=>{
+      this.startIntro();
+    })
+  }
+  ngOnDestroy() {
+    if(this.walkthroughSubscription)
+      this.walkthroughSubscription.unsubscribe();
+  }
+
+  startIntro() {
+    const intro = introJs();
+    intro.setOptions({
+      steps: [
+        {
+          element: '#Help',
+          intro: 'Click here to go to the help page on how to use the app'
+        },
+        {
+          element: '#uploadButton',
+          intro: 'Click here to start uploading documents'
+        }
+      ],
+    });
+    intro.start();
+  }
+  toggleWalkthrough() {
+    this.startIntro();
+  }
 }
