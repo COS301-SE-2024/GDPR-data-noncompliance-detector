f1780110b8dd3797ef2dd77d524fe9ea74b80ff6
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-07-23 17:45:34 +0200

    NER creation script

---
+++ backend/Models/NER_model/create_NER_model.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import spacy 
+import requests 
+import sentencepiece as spm
+from bs4 import BeautifulSoup
+nlp = spacy.load("en_core_web_trf")
+pd.set_option("display.max_rows", 5000)
+
+
+if __name__ == "__main__":
+    res = nlp("")
+    print(len(res.ents))
+
+    print_this = 0
+    for i in res.ents:
+        print_this += 1
+
+    print(f"Set up complete : ",print_this)
+
+    nlp.to_disk('Entity_builder')
