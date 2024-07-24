1bedc84d7a7058996970556b224270222b2686e3
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-07-24 15:17:53 +0200

    implemented message queues

--- backend/backend_entry.py
+++ backend/backend_entry.py
@@ -1,26 +1,52 @@
+import queue
+import threading
 from Document_parser.document_parser import document_parser
 from Detection_Engine.detection_engine import detection_engine
 import sys
+
+file_queue = queue.Queue()
+result_holder = queue.Queue()
+
+def document_parser_worker(path):
+    parser = document_parser()
+    file = parser.process(path)
+    file_queue.put(file) 
+
+def detection_engine_worker():
+    engine = detection_engine()
+    while True:
+        file = file_queue.get()
+        if file is None:
+            break
+        result = engine.process(file)
+        file_queue.task_done() 
+        result_holder.put(result)
+
 
 class backend_entry:
 
     @staticmethod
     def process(path):
+        parser_thread = threading.Thread(target=document_parser_worker, args=(path,))
+        engine_thread = threading.Thread(target=detection_engine_worker)
 
-        parser = document_parser()
-        engine = detection_engine()
-        # path = input("File Name:  ")
-        file = parser.process(path)
-        result = engine.process(file)
-        # print(result)
+        parser_thread.start()
+        engine_thread.start()
 
-        # print(file)
+        parser_thread.join() 
+        file_queue.put(None)
+        engine_thread.join()
+
+        if result_holder.empty():
+            return "No result available"
+
+        result = result_holder.get()
         return result
-    
 
 if __name__ == "__main__":
     try:
-        backend_entry.process()
+        res = backend_entry.process("C:/Users/Mervyn Rangasamy/Documents/2024/COS 301/Capstone/Repo/GDPR-data-noncompliance-detector/backend/mockdata/NCE1.pdf")
+        print(res)
     except SystemExit as e:
         print("An error occurred: ", e)
         sys.exit(1)
