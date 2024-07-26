8c8d43bb015b729e46ce56c52d2988af754bbec9
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-07-26 16:20:46 +0200

    file service fix

--- gnd-app/src/app/services/file.service.ts
+++ gnd-app/src/app/services/file.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class FileService {
-  private apiUrl = 'http://127.0.0.1:8000/reports';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class FileService {
+//   private apiUrl = 'http://127.0.0.1:8000/reports';
 
-  constructor(private http: HttpClient) {}
+//   constructor(private http: HttpClient) {}
 
-  getFiles(directory: string): Observable<string[]> {
-    console.log("Getting files")
-    return this.http.get<string[]>(`${this.apiUrl}?directory=${directory}`);
-  }
-}
+//   getFiles(directory: string): Observable<string[]> {
+//     console.log("Getting files")
+//     return this.http.get<string[]>(`${this.apiUrl}?directory=${directory}`);
+//   }
+// }
