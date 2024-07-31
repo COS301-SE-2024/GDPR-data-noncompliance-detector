87f45d1adc8ea4a077cb077b4b4de43593c3f618
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-07-31 18:18:22 +0200

    added excel image extractor

--- backend/Document_parser/extract_images.py
+++ backend/Document_parser/extract_images.py
@@ -1,6 +1,29 @@
 from pypdf import PdfReader
 import os
 import docx2txt
+from openpyxl import load_workbook
+from openpyxl.drawing.image import Image
+
+def extract_images_from_excel(excel_path):
+    output_dir = "../Detection_Engine/extracted_images/pdf_images"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    workbook = load_workbook(excel_path)
+    
+    for sheet_name in workbook.sheetnames:
+        sheet = workbook[sheet_name]
+        for image in sheet._images:
+            img = image.image
+            
+            img_format = img.format if img.format else 'png'
+            
+            img_filename = f"{sheet_name}_{image.anchor._from.row}_{image.anchor._from.col}.{img_format.lower()}"
+            img_path = os.path.join(output_dir, img_filename)
+            
+            with open(img_path, 'wb') as f:
+                f.write(img._data())
 
 
 def extract_images_from_pdf(dir):
