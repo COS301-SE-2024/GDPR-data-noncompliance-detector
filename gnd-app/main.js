78f4e0d064eadadc9548c945ed7cecf0985e2adf
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-08-09 10:58:39 +0200

    Now the main.js is working fine

--- gnd-app/main.js
+++ gnd-app/main.js
@@ -5,19 +5,19 @@ const path = require('path');
 const fs = require('fs');
 
 // Must delete the following line after testing
-let electronReload;
-try {
-  electronReload = require('electron-reload');
-} catch (err) {
-  console.log('electron-reload not found. Hot reloading will not be available.');
-}
+// let electronReload;
+// try {
+//   electronReload = require('electron-reload');
+// } catch (err) {
+//   console.log('electron-reload not found. Hot reloading will not be available.');
+// }
 
 let apiProcess;
 
 //Must remember to delete this line after being done
-electronReload(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-});
+// electronReload(__dirname, {
+//   electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+// });
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -31,15 +31,15 @@ function createWindow () {
     }
   })
   win.setMenu(null);
-  // win.loadFile(path.join(__dirname, 'dist', 'gnd-app', 'index.html'))
+  win.loadFile(path.join(__dirname, 'dist', 'gnd-app', 'index.html'))
 
   //While fixing the frontend code
-  win.loadURL('http://localhost:4200');
+  // win.loadURL('http://localhost:4200');
   
-  // Open DevTools in development
-  if (process.env.NODE_ENV === 'development') {
-    win.webContents.openDevTools();
-  }
+  // // Open DevTools in development
+  // if (process.env.NODE_ENV === 'development') {
+  //   win.webContents.openDevTools();
+  // }
 
 }
 
@@ -171,141 +171,3 @@ app.on('before-quit', () => {
     apiProcess.kill();
   }
 });
-
-// const { app, BrowserWindow } = require('electron')
-// const { spawn } = require('child_process');
-// const axios = require('axios');
-// const path = require('path');
-// const fs = require('fs');
-
-// let apiProcess;
-
-// function createWindow () {
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     minWidth: 700,
-//     minHeight: 600,
-//     icon: path.join(__dirname, 'src/assets/logo.ico'),
-//     webPreferences: {
-//       nodeIntegration: true,
-//     }
-//   })
-//   win.setMenu(null);
-//   win.loadFile(path.join(__dirname, 'dist', 'gnd-app', 'index.html'))
-// }
-
-// app.whenReady().then(() => {
-//   // startAPI();
-//   startFlaskAPI();
-//   createWindow();
-//   setTimeout(setupWatcher, 100);
-// });
-
-// function startFlaskAPI() {
-//   const apiPath = path.join(__dirname, '..', 'backend');
-//   apiProcess = spawn('python', ['flask_api.py'], { cwd: apiPath });
-
-//   apiProcess.stdout.on('data', (data) => {
-//     console.log(`Flask API stdout: ${data}`);
-//   });
-
-//   apiProcess.stderr.on('data', (data) => {
-//     console.error(`Flask API stderr: ${data}`);
-//   });
-
-//   apiProcess.on('error', (error) => {
-//     console.error(`Failed to start Flask API: ${error}`);
-//   });
-
-//   apiProcess.on('close', (code) => {
-//     console.log(`Flask API process exited with code ${code}`);
-//   });
-// }
-
-
-// function startAPI() {
-//   const apiPath = path.join(__dirname, '..', 'backend');
-//   const api = spawn('uvicorn', ['api:app', '--reload'], {cwd: apiPath});
-
-//   api.stdout.on('data', (data) => {
-//     console.log(`API stdout: ${data}`);
-//   });
-
-//   api.stderr.on('data', (data) => {
-//     console.error(`API stderr: ${data}`);
-//   });
-
-//   api.on('error', (error) => {
-//     console.error(`Failed to start API: ${error}`);
-//   });
-
-//   api.on('close', (code) => {
-//     console.log(`child process exited with code ${code}`);
-//   });
-// }
-
-// function getReceiverPath() {
-//   return new Promise((resolve, reject) => {
-//     const pythonProcess = spawn('python', ['get_absolute_path.py']);
-    
-//     pythonProcess.stdout.on('data', (data) => {
-//       resolve(data.toString().trim());
-//     });
-
-//     pythonProcess.stderr.on('data', (data) => {
-//       reject(data.toString());
-//     });
-
-//     pythonProcess.on('close', (code) => {
-//       if (code !== 0) {
-//         reject(`Python script exited with code ${code}`);
-//       }
-//     });
-//   });
-// }
-
-// function setupWatcher() {
-
-//   // const receiver_path = getReceiverPath();
-//   const watcher = spawn('python', ['../backend/File_monitor/file_watcher.py', '../backend/Receiver', 'pdf,docx,xlsx,xls']);
-
-//   watcher.stdout.on('data', (data) => {
-//     let output = data.toString().trim();
-//     console.log(`Watcher stdout: ${output}`);
-//     const postData = { path: output };
-
-//     const fileName = path.basename(output);
-//     console.log(`Extracted file name: ${fileName}`);
-
-//     const parts = fileName.split('.');
-//     const name = parts[0] + '_report.txt';
-//     const newFileName = name;
-
-//     const outputDir = path.join('../backend/Reports', newFileName);
-
-
-//     axios.post('http://127.0.0.1:8000/new-file', postData, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     })
-//       .then((res) => {
-//         console.log("Alive--------------------------");
-//       output = JSON.stringify(res.data);
-//       console.log("Report successfully created")
-//       const outputDir = path.join('../backend/Reports', newFileName);
-//       fs.writeFileSync(outputDir, output, 'utf8');
-//     })
-//     .catch((error) => {
-//       console.error(`problem with request: ${error.message}`);
-//     });
-//   });
-  
-// }
-
-// app.on('before-quit', () => {
-//   if (apiProcess) {
-//     apiProcess.kill();
-//   }
-// });
