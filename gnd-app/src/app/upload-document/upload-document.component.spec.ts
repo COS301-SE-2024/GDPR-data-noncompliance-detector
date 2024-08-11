3377698705d9e6f54425abdc931b48f742a2b712
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-08-11 16:21:47 +0200

    Test

--- gnd-app/src/app/upload-document/upload-document.component.spec.ts
+++ gnd-app/src/app/upload-document/upload-document.component.spec.ts
@@ -54,58 +54,30 @@ describe('UploadDocumentComponent', () => {
   it('should call onFileSelected and upload file', () => {
     const file = new File(['file content'], 'test.txt', { type: 'text/plain' });
     const input = debugElement.query(By.css('input[type="file"]')).nativeElement;
-  
+
     spyOn(component, 'onFileSelected').and.callThrough();
-  
+
     const dataTransfer = new DataTransfer();
     dataTransfer.items.add(file);
-  
+
     input.files = dataTransfer.files;
     input.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }));
-  
+
     fixture.detectChanges();
-  
+
     expect(component.onFileSelected).toHaveBeenCalled();
     expect(component.fileName).toBe('test.txt');
-  
-    const req = httpMock.expectOne('http://127.0.0.1:8000/file-upload-new');
+
+    const req = httpMock.expectOne('http://127.0.0.1:8000/file-upload');
     expect(req.request.method).toBe('POST');
-    
-    const mockResponse = {
-      fileName: 'test.txt',
-      result: {
-        score: {
-          Status: 1,
-          NER: 5,
-          location: 2,
-          Personal: 3,
-          Financial: 4,
-          Contact: 5,
-          Medical: 6,
-          Ethnic: 7,
-          Biometric: 8,
-          "Consent Agreement": true
-        }
-      }
-    };
-    
-    req.flush(mockResponse);
-  
+    req.flush({ fileName: 'test.txt', result: 'file processed' });
+
     fixture.detectChanges();
-  
+
     expect(component.uploadedFileName).toBe('test.txt');
-    expect(component.documentStatus).toBe(component.docStatus(mockResponse.result.score.Status));
-    expect(component.nerCount).toBe(mockResponse.result.score.NER);
-    expect(component.location).toBe(component.locationStatus(mockResponse.result.score.location));
-    expect(component.personalData).toBe(mockResponse.result.score.Personal);
-    expect(component.financialData).toBe(mockResponse.result.score.Financial);
-    expect(component.contactData).toBe(mockResponse.result.score.Contact);
-    expect(component.medicalData).toBe(mockResponse.result.score.Medical);
-    expect(component.ethnicData).toBe(mockResponse.result.score.Ethnic);
-    expect(component.biometricData).toBe(mockResponse.result.score.Biometric);
-    expect(component.consentAgreement).toBe(component.consentAgreementStatus(mockResponse.result.score["Consent Agreement"]));
-    expect(component.result).toBe('Y');
+    expect(component.result).toBe('file processed'.replace(/\n/g, '<br>'));
   });
+
 it('should display the analysis result in the analysis window', () => {
   const file = new File(['file content'], 'test.txt', { type: 'text/plain' });
   const input = debugElement.query(By.css('input[type="file"]')).nativeElement;
