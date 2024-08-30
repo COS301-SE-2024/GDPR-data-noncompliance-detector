e2ff8fd928d3db19b8c7218871b80ac99ecb437d
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-08-30 15:36:27 +0200

    fixing the routing

--- gnd-admin/src/app/app-routing.module.ts
+++ gnd-admin/src/app/app-routing.module.ts
@@ -12,9 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- gnd-admin/src/app/login/login.component.ts
+++ gnd-admin/src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
