71a6e2a3909b18e16d5902ef512aee6f840ea03d
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-08-30 15:27:48 +0200

    Structure of the landing page

---
+++ gnd-admin/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
