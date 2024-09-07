a5133dd69d6ff95e4d26b77494e2050bfa09fd62
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-07 13:45:01 +0200

    progress

--- gnd-admin/src/app/login/login.component.ts
+++ gnd-admin/src/app/login/login.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showLoginForm = true;
+  showRegisterForm = false;
 
+  // Login form fields
+  loginEmail = '';
+  loginPassword = '';
+
+  // Register form fields
+  firstName = '';
+  lastName = '';
+  registerEmail = '';
+  registerPassword = '';
+  confirmPassword = '';
+  agreeTerms = false;
+
+  toggleForms() {
+    this.showRegisterForm = !this.showRegisterForm;
+  }
+
+  onLoginSubmit() {
+    // Implement login logic here
+    console.log('Login submitted', this.loginEmail, this.loginPassword);
+  }
+
+  onRegisterSubmit() {
+    // Implement registration logic here
+    console.log('Register submitted', this.firstName, this.lastName, this.registerEmail, this.registerPassword);
+  }
 }
