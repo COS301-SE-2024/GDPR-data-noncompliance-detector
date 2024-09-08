f64611bb5ef3d07ccd2acaac6ed6452f400751c6
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-08 23:16:25 +0200

    Basic landing page is complete

--- gnd-admin/src/app/landing/landing.component.ts
+++ gnd-admin/src/app/landing/landing.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
 
