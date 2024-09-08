b31e2eb19793a79dc26490982456be35e932785f
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-08 16:18:10 +0200

    Basic structure working all things are static for now.
    I will be working on making sure now that everything looks good. Then I will be moving on to how everything works with real data from the database supabase.

--- gnd-admin/src/app/login/login.component.ts
+++ gnd-admin/src/app/login/login.component.ts
@@ -1,33 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   showLoginForm = true;
-  showRegisterForm = false;
 
   // Login form fields
   loginEmail = '';
   loginPassword = '';
-
-  // Register form fields
-  firstName = '';
-  lastName = '';
-  registerEmail = '';
-  registerPassword = '';
-  confirmPassword = '';
-  agreeTerms = false;
-
-  // toggleForms() {
-  //   this.showRegisterForm = !this.showRegisterForm;
-  // }
 
   onLoginSubmit() {
     // Implement login logic here
--- gnd-admin/src/app/register/register.component.ts
+++ gnd-admin/src/app/register/register.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  showLoginForm = true;
   showRegisterForm = false;
-
-  // Login form fields
-  loginEmail = '';
-  loginPassword = '';
 
   // Register form fields
   firstName = '';
@@ -24,17 +20,7 @@ export class RegisterComponent {
   registerPassword = '';
   confirmPassword = '';
   agreeTerms = false;
-
-  toggleForms() {
-    this.showRegisterForm = !this.showRegisterForm;
-  }
-
-  onLoginSubmit() {
-    // Implement login logic here
-    console.log('Login submitted', this.loginEmail, this.loginPassword);
-  }
-
-
+  
   onRegisterSubmit() {
     // Implement registration logic here
     console.log('Register submitted', this.firstName, this.lastName, this.registerEmail, this.registerPassword);
