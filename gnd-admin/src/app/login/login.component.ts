1cd98ee43b42c80c5958129e374225d1b6de46a3
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-08 18:53:13 +0200

    Cleaning up comments
    Dead code that was commented

--- gnd-admin/src/app/login/login.component.ts
+++ gnd-admin/src/app/login/login.component.ts
@@ -1,28 +1,3 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import {RouterLink} from "@angular/router";
-//
-// @Component({
-//   selector: 'app-login',
-//   standalone: true,
-//   imports: [CommonModule, FormsModule, RouterLink],
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css']
-// })
-// export class LoginComponent {
-//   showLoginForm = true;
-//
-//   // Login form fields
-//   loginEmail = '';
-//   loginPassword = '';
-//
-//   onLoginSubmit() {
-//     // Implement login logic here
-//     console.log('Login submitted', this.loginEmail, this.loginPassword);
-//   }
-//
-// }
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
