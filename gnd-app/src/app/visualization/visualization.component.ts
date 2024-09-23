9524bc04eb01dadcf5161abf701b5dc3021bcbe1
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-09-23 22:21:08 +0200

    added data-viz page

---
+++ gnd-app/src/app/visualization/visualization.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualization',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualization.component.html',
+  styleUrl: './visualization.component.css'
+})
+export class VisualizationComponent {
+
+}
