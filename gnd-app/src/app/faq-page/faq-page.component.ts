b63028bcc06ef9d3b8fe6c38185fd20e5842a0f0
Nevin-Thomas <106274286+Nevin-Thomas@users.noreply.github.com>
2024-09-25 12:02:09 +0200

    Update frontend/editing-faq-page
    Sorted the drop down functionality.

--- gnd-app/src/app/faq-page/faq-page.component.ts
+++ gnd-app/src/app/faq-page/faq-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule for ngClass
 
 @Component({
   selector: 'app-faq-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './faq-page.component.html',
   styleUrl: './faq-page.component.css'
 })
 export class FaqPageComponent {
+  activeDropdown: string = '';  // To store the currently active dropdown
 
+  // Function to toggle the dropdown
+  toggleDropdown(id: string) {
+    this.activeDropdown = this.activeDropdown === id ? '' : id;  // Toggle logic
+  }
 }
