93510425beb4dd744ef591387662c00956b88099
Yeshlen Moodley <yeshlenmoodley01@gmail.com>
2024-09-25 22:35:03 +0200

    Component Test

--- gnd-app/src/app/outlook-inbox/outlook-inbox.component.spec.ts
+++ gnd-app/src/app/outlook-inbox/outlook-inbox.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OutlookInboxComponent } from './outlook-inbox.component';
 
 describe('OutlookInboxComponent', () => {
@@ -8,7 +8,9 @@ describe('OutlookInboxComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OutlookInboxComponent]
+      imports: [OutlookInboxComponent,
+        HttpClientTestingModule,
+      ]
     })
     .compileComponents();
 
