a4e664e4209492e02664023950fec36e993fe661
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-09-26 09:40:40 +0200

    fixed NER

--- backend/Detection_Engine/report_generation_layer.py
+++ backend/Detection_Engine/report_generation_layer.py
@@ -72,16 +72,16 @@ class report_generation_layer:
 
     def ner_report_generation(self, text):
         res1 = self.classification_layer.run_NER_model(text)
-        entities = 0
-        names = []
+        # entities = 0
+        # names = []
 
-        for text,label in res1:
-            if label == 'PERSON':
-                if text not in names:
-                    names.append(text)
-                    entities += 1
+        # for text,label in res1:
+        #     if label == 'PERSON':
+        #         if text not in names:
+        #             names.append(text)
+        #             entities += 1
     
-        return entities
+        return res1
     
 #----------------------------------------------------------REPORT GEN END------------------------------------------------------------------#
     
