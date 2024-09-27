76015f5e90f07c703ac2ea994c6b12955cf8c165
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-27 13:14:20 +0200

    Reactive and dynamic navbar

--- gnd-admin/src/app/app.component.ts
+++ gnd-admin/src/app/app.component.ts
@@ -1,3 +1,36 @@
+// import { CommonModule } from '@angular/common';
+// import { Component, OnInit } from '@angular/core';
+// import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+// import { initFlowbite } from 'flowbite';
+// import { AuthService } from './services/auth.service';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'] 
+// })
+// export class AppComponent implements OnInit {
+//   title = 'gnd-admin';
+//   isLoggedIn =false;
+//   constructor(private authService: AuthService, private router: Router) {
+//     this.authService.isLoggedIn.subscribe(status => {
+//       this.isLoggedIn = status;
+//     });
+//   }
+
+//   logout(){
+//     this.authService.logout();
+//     this.router.navigate(['/landing']);
+
+//   }
+
+//   ngOnInit(): void {
+//     initFlowbite();
+//   }
+  
+// }
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
@@ -13,21 +46,29 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'gnd-admin';
-  isLoggedIn =false;
+  isLoggedIn = false;
+  isMenuOpen = false; // Add this property
+
   constructor(private authService: AuthService, private router: Router) {
+    // Subscribe to the isLoggedIn status from AuthService
     this.authService.isLoggedIn.subscribe(status => {
       this.isLoggedIn = status;
     });
   }
 
-  logout(){
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Toggle the menu state
+  }
+
+  logout() {
     this.authService.logout();
     this.router.navigate(['/landing']);
-
   }
 
   ngOnInit(): void {
+    // Initialize Flowbite
     initFlowbite();
   }
+
   
 }
