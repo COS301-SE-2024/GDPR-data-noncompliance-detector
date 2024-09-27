90858216376a9104bb9e31b54039ff77131beff4
Yeshlen Moodley <yeshlenmoodley01@gmail.com>
2024-09-27 15:15:30 +0200

    Paths: Biometric

--- backend/Detection_Engine/biometric_detection.py
+++ backend/Detection_Engine/biometric_detection.py
@@ -16,22 +16,31 @@ def resource_path(relative_path):                                               
     else:
         return os.path.join(os.path.abspath("."), relative_path)
     
-def create_directories():
-    directories = [
-        resource_path('./local_model'),
-        resource_path('./Detection_Engine/extracted_images/pdf_images'),
-        resource_path('./Detection_Engine/extracted_images/docx_images'),
-        resource_path('./Detection_Engine/extracted_images/xlsx_images')
-    ]
+EXTRACTED_PDF_FOLDER = os.path.expanduser("~/Documents/GND/extracted_images/pdf_images")
+os.makedirs(EXTRACTED_PDF_FOLDER, exist_ok=True)
+
+EXTRACTED_DOCX_FOLDER = os.path.expanduser("~/Documents/GND/extracted_images/docx_images")
+os.makedirs(EXTRACTED_DOCX_FOLDER, exist_ok=True)
+
+EXTRACTED_XLSX_FOLDER = os.path.expanduser("~/Documents/GND/extracted_images/xlsx_images")
+os.makedirs(EXTRACTED_XLSX_FOLDER, exist_ok=True)
     
-    for directory in directories:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+# def create_directories():
+#     directories = [
+#         resource_path('./local_model'),
+#         resource_path('./Detection_Engine/extracted_images/pdf_images'),
+#         resource_path('./Detection_Engine/extracted_images/docx_images'),
+#         resource_path('./Detection_Engine/extracted_images/xlsx_images')
+#     ]
+    
+#     for directory in directories:
+#         if not os.path.exists(directory):
+#             os.makedirs(directory)
 
 class biometric_detection:
     
-    def __init__(self):
-        create_directories()
+    # def __init__(self):
+        # create_directories()
 
     def save_model_local():
         processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
@@ -75,7 +84,8 @@ class biometric_detection:
             # extract_images_from_pdf(pdf_path)
 
             # images = [f'./Detection_Engine/extracted_images/pdf_images/{i}' for i in os.listdir('./Detection_Engine/extracted_images/pdf_images')  if i.endswith('.png') or i.endswith('.jpg')]
-            images_dir = resource_path('./Detection_Engine/extracted_images/pdf_images')
+            # images_dir = resource_path('./Detection_Engine/extracted_images/pdf_images')
+            images_dir = EXTRACTED_PDF_FOLDER
             images = [os.path.join(images_dir, i) for i in os.listdir(images_dir) if i.endswith('.png') or i.endswith('.jpg')]
             
             output = []
@@ -85,7 +95,8 @@ class biometric_detection:
                 output.append(self.biometric_detect_people(image))
             
             # png_files = glob.glob(os.path.join("./Detection_Engine/extracted_images/pdf_images", '*.png'))
-            png_files = glob.glob(os.path.join(resource_path('./Detection_Engine/extracted_images/pdf_images'), '*.png'))
+            # png_files = glob.glob(os.path.join(resource_path('./Detection_Engine/extracted_images/pdf_images'), '*.png'))
+            png_files = glob.glob(os.path.join(EXTRACTED_PDF_FOLDER, '*.png'))
             for file in png_files:
                 os.remove(file)
 
@@ -95,7 +106,8 @@ class biometric_detection:
             # extract_images_from_docx(pdf_path)
             
             # images = [f'./Detection_Engine/extracted_images/docx_images/{i}' for i in os.listdir('./Detection_Engine/extracted_images/docx_images')  if i.endswith('.png') or i.endswith('.jpg')]
-            images_dir = resource_path('./Detection_Engine/extracted_images/docx_images')
+            # images_dir = resource_path('./Detection_Engine/extracted_images/docx_images')
+            images_dir = EXTRACTED_DOCX_FOLDER
             images = [os.path.join(images_dir, i) for i in os.listdir(images_dir) if i.endswith('.png') or i.endswith('.jpg')]
             
             output = []
@@ -105,7 +117,8 @@ class biometric_detection:
                 output.append(self.biometric_detect_people(image))
             
             # png_files = glob.glob(os.path.join("./Detection_Engine/extracted_images/docx_images", '*.png'))
-            png_files = glob.glob(os.path.join(resource_path('./Detection_Engine/extracted_images/docx_images'), '*.png'))
+            # png_files = glob.glob(os.path.join(resource_path('./Detection_Engine/extracted_images/docx_images'), '*.png'))
+            png_files = glob.glob(os.path.join(EXTRACTED_DOCX_FOLDER, '*.png'))
             for file in png_files:
                 os.remove(file)
 
@@ -116,7 +129,8 @@ class biometric_detection:
             # images = [f'./Detection_Engine/extracted_images/xlsx_images/{i}' for i in os.listdir('./Detection_Engine/extracted_images/xlsx_images')  if i.endswith('.png') or i.endswith('.jpg')]
             # images = [f'extracted_images/xlsx_images/{i}' for i in os.listdir('extracted_images/xlsx_images')]
             
-            images_dir = resource_path('./Detection_Engine/extracted_images/xlsx_images')
+            # images_dir = resource_path('./Detection_Engine/extracted_images/xlsx_images')
+            images_dir = EXTRACTED_XLSX_FOLDER
             images = [os.path.join(images_dir, i) for i in os.listdir(images_dir) if i.endswith('.png') or i.endswith('.jpg')]
 
             output = []
@@ -126,7 +140,8 @@ class biometric_detection:
                 output.append(self.biometric_detect_people(image))
             
             # png_files = glob.glob(os.path.join("./Detection_Engine/extracted_images/xlsx_images", '*.png'))
-            png_files = glob.glob(os.path.join(resource_path('./Detection_Engine/extracted_images/xlsx_images'), '*.png'))
+            # png_files = glob.glob(os.path.join(resource_path('./Detection_Engine/extracted_images/xlsx_images'), '*.png'))
+            png_files = glob.glob(os.path.join(EXTRACTED_XLSX_FOLDER, '*.png'))
             for file in png_files:
                 os.remove(file)
 
