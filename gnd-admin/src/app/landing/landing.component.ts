f35ed63a7ead74e0ba0294ed5145afceb82ebe75
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-27 12:47:15 +0200

    Looks much better now

--- gnd-admin/src/app/landing/landing.component.ts
+++ gnd-admin/src/app/landing/landing.component.ts
@@ -1,26 +1,4 @@
-// import { Component,  OnInit  } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule } from '@angular/router';
-
-// @Component({
-//   selector: 'app-landing',
-//   standalone: true,
-//   imports: [CommonModule, RouterModule],
-//   templateUrl: './landing.component.html',
-//   styleUrls: ['./landing.component.css']
-// })
-// export class LandingComponent implements OnInit {
-//   constructor() {}
-
-//   ngOnInit() {
-    
-//   }
-
-  
-
-  
-// }
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -31,26 +9,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit {
-  
-  // Object to keep track of the expanded state of each card
-  expandedCards: { [key: string]: boolean } = {};
+export class LandingComponent {
 
-  constructor() {}
-
-  ngOnInit() {
-    // Initialize all cards to be collapsed
-    this.expandedCards = {
-      gdpr1: false,
-      gdpr2: false,
-      gdpr3: false,
-      gdpr4: false,
-      gdpr5: false
-    };
-  }
-
-  // Method to toggle the expansion of cards
-  toggleExpand(cardId: string) {
-    this.expandedCards[cardId] = !this.expandedCards[cardId];
-  }
 }
