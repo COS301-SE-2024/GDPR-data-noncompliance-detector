fbfde7ee6d93dd72d97a858bcecf491638ea5094
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-09-28 15:52:30 +0200

    annotation component

---
+++ gnd-app/src/app/annotation/annotation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-annotation',
+  standalone: true,
+  imports: [],
+  templateUrl: './annotation.component.html',
+  styleUrl: './annotation.component.css'
+})
+export class AnnotationComponent {
+
+}
