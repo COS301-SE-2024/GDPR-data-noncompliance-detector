58a11655c45df227822dbe7e731fa2eb8b3c4929
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-29 23:26:02 +0200

    Checking

--- gnd-admin/src/environments/environment.prod.ts
+++ gnd-admin/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
-// const supabaseUrl: string | undefined = process.env['SUPABASE_URL'];
-// const supabaseKey: string | undefined = process.env['SUPABASE_KEY'];
+const supabaseUrl: string | undefined = process.env['SUPABASE_URL'];
+const supabaseKey: string | undefined = process.env['SUPABASE_KEY'];
 
-// const url: string = supabaseUrl ?? 'defaultSupabaseUrl';
-// const key: string = supabaseKey ?? 'defaultSupabaseKey';
+const url: string = supabaseUrl ?? 'defaultSupabaseUrl';
+const key: string = supabaseKey ?? 'defaultSupabaseKey';
 
 export const environment = {
   production: true,
-  supabaseUrl:"https://oadcyxznbhdrzsutusbh.supabase.co",
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hZGN5eHpuYmhkcnpzdXR1c2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4MTAyNDUsImV4cCI6MjA0MTM4NjI0NX0.DLDq7NyjhmEv9V1bRERp2e5XT0-qFdBjWN3BNed6EfY",
+  supabaseUrl:url,
+  supabaseKey: key,
 };
