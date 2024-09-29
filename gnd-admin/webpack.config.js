fa433920220d0d6960c2959deda46f82c1a45d55
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-29 23:22:29 +0200

    Testing

--- gnd-admin/webpack.config.js
+++ gnd-admin/webpack.config.js
@@ -1,7 +1,22 @@
+// const Dotenv = require('dotenv-webpack');
+
+// module.exports = {
+//   plugins: [
+//     new Dotenv()
+//   ]
+// };
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  plugins: [
-    new Dotenv()
-  ]
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      new Dotenv() // Use dotenv for environment variables
+    ],
+  });
 };
