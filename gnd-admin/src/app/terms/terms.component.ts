a97c97ca9497efdfb73afff2ef54b768801bbc96
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-09-29 02:54:20 +0200

    working version

--- gnd-admin/src/app/terms/terms.component.ts
+++ gnd-admin/src/app/terms/terms.component.ts
@@ -1,33 +1,17 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-terms',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './terms.component.html',
-//   styleUrl: './terms.component.css'
-// })
-// export class TermsComponent {
-//   closeModal() {
-//     const modal = document.getElementById('termsModal');
-//     if (modal) {
-//       modal.style.display = 'none';
-//     }
-//   }
-// }
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
+  imports: [],
   templateUrl: './terms.component.html',
-  styleUrls: ['./terms.component.css']
+  styleUrl: './terms.component.css'
 })
 export class TermsComponent {
   closeModal() {
     const modal = document.getElementById('termsModal');
     if (modal) {
-      modal.classList.add('hidden');
+      modal.style.display = 'none';
     }
   }
 }
