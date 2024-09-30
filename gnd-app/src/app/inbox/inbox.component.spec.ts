9e4c248eed1685a26ba03cc55f248d1be21f8063
Yeshlen Moodley <yeshlenmoodley01@gmail.com>
2024-09-30 03:01:31 +0200

    Anno, Upload

--- gnd-app/src/app/inbox/inbox.component.spec.ts
+++ gnd-app/src/app/inbox/inbox.component.spec.ts
@@ -1,27 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import { InboxComponent } from './inbox.component';
-
-// describe('InboxComponent', () => {
-//   let component: InboxComponent;
-//   let fixture: ComponentFixture<InboxComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [InboxComponent]
-//     })
-//     .compileComponents();
-
-//     fixture = TestBed.createComponent(InboxComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
