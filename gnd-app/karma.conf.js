10ade9bbe5f4d42fab6258c54b12ff8a1ec15370
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2024-09-30 04:48:59 +0200

    pythoncom

--- gnd-app/karma.conf.js
+++ gnd-app/karma.conf.js
@@ -1,22 +1,29 @@
 module.exports = function (config) {
-    config.set({
-    //   basePath: '',
-    //   frameworks: ['jasmine', '@angular-devkit/build-angular', 'sinon'],
+  config.set({
+    // other configurations like basePath, frameworks, etc.
+    
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
 
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage-istanbul-reporter'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-     
-      browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'], // Use ChromeHeadless instead of Chrome
-      customLaunchers: {
-        ChromeHeadlessNoSandbox: {
-          base: 'ChromeHeadless',
-          flags: ['--no-sandbox']
-        }
-      },
-    });
-  };
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'coverage'), // Output directory for coverage reports
+      reports: ['lcovonly', 'text-summary'],  // Generates lcov report and text-summary for console
+      fixWebpackSourcePaths: true
+    },
+
+    reporters: ['progress', 'coverage-istanbul'],
+
+    browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
+  });
+};
