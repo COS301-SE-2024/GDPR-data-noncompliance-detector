6dc5543d2e56e7883dad167e5acbc3b2da075e3c
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-10-13 21:26:00 +0200

    concurrent running

--- backend/Detection_Engine/report_generation_layer.py
+++ backend/Detection_Engine/report_generation_layer.py
@@ -1,7 +1,6 @@
 from Detection_Engine.text_classification_layer import text_classification_layer
 from Detection_Engine.biometric_detection import biometric_detection
 from Detection_Engine.lang_detection import location_finder
-from langcodes import Language
 
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
