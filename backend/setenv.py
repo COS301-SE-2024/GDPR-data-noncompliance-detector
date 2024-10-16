dd084e082d48f7f133a915e6882aaaab514b7224
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-10-16 18:23:34 +0200

    updates

--- backend/setenv.py
+++ backend/setenv.py
@@ -1,7 +1,11 @@
 import os
 
-# Set the environment variable
-# os.environ['GND_ENCRYPTION_KEY'] = 'IWIllreplacethislaterIWIllreplac'
+# Define the environment variable and its value
+env_var_name = 'SYS VAR x64'
+env_var_value = 'qpn0Dx9zaqjQq9Lgc4b5seXOQsus3ZGu'
+
+# Use setx to set the environment variable permanently
+os.system(f'setx "{env_var_name}" "{env_var_value}"')
 
 # Optionally, you can print it to verify
-print(f"ENCRYPTION_KEY set to: {os.getenv('GND_ENCRYPTION_KEY')}")
+print(f'{env_var_name} set to: {env_var_value}')
