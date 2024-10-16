7e7832ee45c3c191709fb1f90caab5dda5f8f40f
u21598012 <133052473+u21598012@users.noreply.github.com>
2024-10-16 13:39:33 +0200

    updated RAG

--- backend/Detection_Engine/report_generation_layer.py
+++ backend/Detection_Engine/report_generation_layer.py
@@ -188,8 +188,8 @@ class report_generation_layer:
             categories.append('Biometric Data')
 
         rag_res, rag_count = self.classification_layer.run_RAG(categories)
-        result = "The following GDPR articles are potentially violated: " + ", ".join(rag_res)
-        return result, rag_count
+        # result = "The following GDPR articles are potentially violated: " + ", ".join(rag_res)
+        return rag_res, rag_count
 
 
 
